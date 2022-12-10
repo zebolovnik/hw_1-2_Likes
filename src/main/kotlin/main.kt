dcd8247a801fbfdeaf6c3fc509e85eaeda27b34b
Nik Ze <zebolovnik@yandex.ru>
2022-12-11 08:05:10 +1000

    Fix val correctPronounce

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,13 +1,10 @@
 package ru.netology
 
 fun main() {
-    val likes = 111
+    val likes = 2111
 
-val correctPronounce = if (likes == 1 || likes % 10 == 1) "Человеку" else if (likes == 11 || likes % 100 == 11) "Людям"
-    else "Людям"
-
-/*    val correctPronounce = if ((likes == 1 || likes % 10 == 1) &&
-        (likes != 11 || likes % 100 != 11)) "Человеку" else "Людям" */
+    val correctPronounce = if ((likes == 1 || likes % 10 == 1) &&
+        !(likes % 100 == 11 || likes == 11)) "Человеку" else "Людям"
 
     println("Понравилось $likes $correctPronounce")
 }
